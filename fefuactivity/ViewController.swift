2dc1d4107e2240eb841360f0fc070a0146e8a3a4
SofiShark <sofiamaster2001@gmail.com>
2022-01-18 06:12:06 +1000

    Project

--- fefuactivity/ViewController.swift
+++ fefuactivity/ViewController.swift
@@ -1,14 +1,6 @@
-//
-//  ViewController.swift
-//  fefuactivity
-//
-//  Created by Andrew L on 04.10.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
     }
